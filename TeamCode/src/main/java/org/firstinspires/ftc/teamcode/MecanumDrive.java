cea5429dffb701e9345aecb27cc1b4079e0ed6e2
uanavarathan <uanavarathan@medqia.com>
2025-10-01 22:46:33 -0700

    updated tuning parameters and fixed robot and field orientated code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -73,12 +73,12 @@ public final class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = 0.00197698195;
-        public double lateralInPerTick = 0.0017438843161299744;
-        public double trackWidthTicks = 5379.494665638308;
+        public double lateralInPerTick = 0.0016551667373066582;
+        public double trackWidthTicks = 5787.11291194599;
 
         // feedforward parameters (in tick units)
-        public double kS = 0.7860179094124928;
-        public double kV = 0.00030578213551861103;
+        public double kS = 0.8277631564939507;
+        public double kV = 0.0002678771637436323;
         public double kA = 0.00001;
 
         // path profile parameters (in inches)
@@ -91,9 +91,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 4.0;
-        public double lateralGain = 3.0;
-        public double headingGain = 3.0; // shared with turn
+        public double axialGain = 5.0;
+        public double lateralGain = 5.0;
+        public double headingGain = 4.0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/MecanumFieldOrientedDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/MecanumFieldOrientedDrive.java
@@ -1,20 +1,88 @@
 package org.firstinspires.ftc.teamcode.opmodes.Teleop;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name="Field Orientated Drive", group="TeleOp") // FIELD ORIENTATED
-
 public class MecanumFieldOrientedDrive extends OpMode {
 
-    FieldCentricDrive fieldCentricDrive = new FieldCentricDrive();
 
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+
+    private IMU imu;
     double forward, strafe, rotate;
 
     @Override
-    public void init(){
-        fieldCentricDrive.init(hardwareMap);
+    public void init() {
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
 
+
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+
+        imu = hardwareMap.get(IMU.class,"imu");
+
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP); //change depending on robot
+
+        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
+
+
+
+        telemetry.addData("Status", "Initialized");
+    }
+
+    public void drive(double forward, double strafe, double rotate) {
+        double frontLeftPower = forward + strafe + rotate;
+        double backLeftPower  = forward - strafe + rotate;
+        double frontRightPower = forward - strafe - rotate;
+        double backRightPower  = forward + strafe - rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;
+
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        leftFront.setPower( maxSpeed * (frontLeftPower / maxPower));
+        leftBack.setPower( maxSpeed * (backLeftPower / maxPower));
+        rightFront.setPower( maxSpeed * (frontRightPower / maxPower));
+        rightBack.setPower( maxSpeed * (backRightPower / maxPower));
+
+        telemetry.addData("FL", frontLeftPower);
+        telemetry.addData("FR", frontRightPower);
+        telemetry.addData("BL", backLeftPower);
+        telemetry.addData("BR", backRightPower);
+        telemetry.update();
+    }
+
+    public void driveFieldRelative(double forward, double strafe, double rotate) {
+        double theta = Math.atan2(forward, strafe);
+        double r = Math.hypot(strafe, forward);
+
+        theta = AngleUnit.normalizeRadians(theta -
+                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+        double newForward = r * Math.sin(theta);
+        double newStrafe = r * Math.cos(theta);
+
+        newStrafe = newStrafe * 1.1; // <-- scales strafing to counter imperfect mecanum movement
+
+        this.drive(newForward, newStrafe, rotate);
     }
 
     @Override
@@ -23,7 +91,7 @@ public class MecanumFieldOrientedDrive extends OpMode {
         strafe = gamepad1.left_stick_x;
         rotate = gamepad1.right_stick_x;
 
-        fieldCentricDrive.driveFieldRelative(forward, strafe, rotate);
+        this.driveFieldRelative(forward, strafe, rotate);
 
     }
 }
