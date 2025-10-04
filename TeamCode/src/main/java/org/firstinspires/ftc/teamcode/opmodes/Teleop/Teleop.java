1a08aae03f194a3a374eda51c327bd9da7f552c7
ShasshU <143851930+ShasshU@users.noreply.github.com>
2025-10-04 10:21:18 -0700

    updated file name to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/Teleop.java
@@ -4,14 +4,12 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-@TeleOp(name="Field Orientated Drive", group="TeleOp") // FIELD ORIENTATED
-public class MecanumFieldOrientedDrive extends OpMode {
+@TeleOp(name="Teleop", group="TeleOp") // FIELD ORIENTATED
+public class Teleop extends OpMode {
 
 
     private DcMotor leftFront;
