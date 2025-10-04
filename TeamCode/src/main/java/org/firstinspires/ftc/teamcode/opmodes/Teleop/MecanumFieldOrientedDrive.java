05bfc00c6d5be362595565bacc99471a264f3eeb
ShasshU <143851930+ShasshU@users.noreply.github.com>
2025-10-04 10:20:04 -0700

    updated field orientated drive and implemented shooter and intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/MecanumFieldOrientedDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop/MecanumFieldOrientedDrive.java
@@ -1,68 +1,132 @@
 package org.firstinspires.ftc.teamcode.opmodes.Teleop;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name="Field Orientated Drive", group="TeleOp") // FIELD ORIENTATED
-
 public class MecanumFieldOrientedDrive extends OpMode {
 
-    public DcMotor intake;
-    public DcMotorEx flywheel1;
-    public DcMotor flywheel2;
-    private boolean lastA = false; // remembers previous A button state
+
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+
+    private DcMotor flywheel1;
+
+    private DcMotor flywheel2;
+
+    private DcMotor intake;
+
+    private boolean lastA = false;
+
+
     boolean intakeOn = false;
-    public static double targetVelocity = 3000;
 
-    FieldCentricDrive fieldCentricDrive = new FieldCentricDrive();
-
+    private IMU imu;
     double forward, strafe, rotate;
 
     @Override
-    public void init(){
-        fieldCentricDrive.init(hardwareMap);
+    public void init() {
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
         intake = hardwareMap.get(DcMotor.class, "intake");
-        flywheel1 = hardwareMap.get(DcMotorEx.class, "flywheel1");
+        flywheel1 = hardwareMap.get(DcMotor.class, "flywheel1");
         flywheel2 = hardwareMap.get(DcMotor.class, "flywheel2");
-
-        flywheel1.setDirection(DcMotor.Direction.FORWARD);
-        flywheel2.setDirection(DcMotor.Direction.REVERSE);
 
         flywheel1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         flywheel2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+
+        imu = hardwareMap.get(IMU.class,"imu");
+
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP); //change depending on robot
+
+        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
+
+
+
+        telemetry.addData("Status", "Initialized");
     }
 
+    public void drive(double forward, double strafe, double rotate) {
+        double frontLeftPower = forward + strafe + rotate;
+        double backLeftPower  = forward - strafe + rotate;
+        double frontRightPower = forward - strafe - rotate;
+        double backRightPower  = forward + strafe - rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;
+
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        leftFront.setPower( maxSpeed * (frontLeftPower / maxPower));
+        leftBack.setPower( maxSpeed * (backLeftPower / maxPower));
+        rightFront.setPower( maxSpeed * (frontRightPower / maxPower));
+        rightBack.setPower( maxSpeed * (backRightPower / maxPower));
+
+        telemetry.addData("FL", frontLeftPower);
+        telemetry.addData("FR", frontRightPower);
+        telemetry.addData("BL", backLeftPower);
+        telemetry.addData("BR", backRightPower);
+        telemetry.update();
+    }
+
+    public void driveFieldRelative(double forward, double strafe, double rotate) {
+        double theta = Math.atan2(forward, strafe);
+        double r = Math.hypot(strafe, forward);
+
+        theta = AngleUnit.normalizeRadians(theta -
+                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+        double newForward = r * Math.sin(theta);
+        double newStrafe = r * Math.cos(theta);
+
+        newStrafe = newStrafe * 1.1; // <-- scales strafing to counter imperfect mecanum movement
+
+        this.drive(newForward, newStrafe, rotate);
+    }
 
     @Override
-    public void loop(){
+    public void loop() {
         forward = -gamepad1.left_stick_y;
         strafe = gamepad1.left_stick_x;
         rotate = gamepad1.right_stick_x;
 
-        fieldCentricDrive.driveFieldRelative(forward, strafe, rotate);
+        this.driveFieldRelative(forward, strafe, rotate);
 
-        if(gamepad1.a && !lastA) {
+        if (gamepad1.a && !lastA) {
             intakeOn = !intakeOn;
         }
         lastA = gamepad1.a;
 
-        if(intakeOn) {
+        if (intakeOn) {
             intake.setPower(1.0);
-        }
-        else {
+        } else {
             intake.setPower(0.0);
         }
-        if (gamepad1.b) {
-            // Run the master motor at the target velocity
-            flywheel1.setVelocity(targetVelocity);
-            // Have the slave motor mirror the master's power
-            flywheel2.setPower(flywheel1.getPower());
+        if (gamepad1.right_bumper) {
+            flywheel1.setPower(0.5);
+            flywheel1.setPower(0.5);
         } else {
-            // Stop both motors
-            flywheel1.setVelocity(0);
+            flywheel1.setPower(0);
             flywheel2.setPower(0);
+        }
     }
-}
-}
+    }
