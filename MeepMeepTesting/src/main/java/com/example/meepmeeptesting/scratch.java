9bb94196477557e854cd50ab09c615097d44b553
Y-K <wdecrypt0r@gmail.com>
2025-10-26 10:35:41 -0700

    Created Auton Paths
    Did with Maxwell, but don't know his github username

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/scratch.java
@@ -0,0 +1,48 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+//  RedFarAutoLeave
+
+public class scratch {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(700);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+        Pose2d startPos = new Pose2d(60, -22, 0);
+        myBot.runAction(myBot.getDrive().actionBuilder(startPos)
+                .lineToX(50)
+                .splineTo(new Vector2d(-15,15), Math.toRadians(135))
+                .splineToLinearHeading(new Pose2d(11,34, Math.toRadians(90)), Math.toRadians(90))
+                .lineToY(51)
+                .lineToY(25)
+                .splineTo(new Vector2d(-15,15), Math.toRadians(135))
+                .splineToLinearHeading(new Pose2d(-12,34, Math.toRadians(90)), Math.toRadians(90))
+                .lineToY(51)
+                .lineToY(25)
+                .splineTo(new Vector2d(-15,15), Math.toRadians(135))
+                .splineToLinearHeading(new Pose2d(35,34, Math.toRadians(90)), Math.toRadians(90))
+                .lineToY(51)
+                .lineToY(25)
+                .splineTo(new Vector2d(-15,15), Math.toRadians(135))
+
+//                .splineTo(new Vector2d(35,50), Math.toRadians(90))
+//                .splineToLinearHeading(new Pose2d(14,15,Math.toRadians(90)), Math.toRadians(270))
+//                .splineToLinearHeading(new Pose2d(12,55,Math.toRadians(90)), Math.toRadians(270))
+//                .splineToLinearHeading(new Pose2d(-15,-15,Math.toRadians(225)), Math.toRadians(270))
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/scratch3.java
@@ -0,0 +1,52 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+// RedNearAutoLeave
+
+public class scratch3 {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(700);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+        Pose2d startPos = new Pose2d(-28, -22, Math.toRadians(180));
+        myBot.runAction(myBot.getDrive().actionBuilder(startPos)
+                .lineToX(0)
+                .splineTo(new Vector2d(-15, 15), Math.toRadians(135))
+                .splineToLinearHeading(new Pose2d(-12, 30, Math.toRadians(90)), Math.toRadians(270))
+                .lineToY(51)
+                .lineToY(30)
+                .splineTo(new Vector2d(-15, 15), Math.toRadians(135))
+                .splineToLinearHeading(new Pose2d(11, 25, Math.toRadians(90)), Math.toRadians(270))
+                .lineToY(51)
+                .lineToY(30)
+                .splineTo(new Vector2d(-15, 15), Math.toRadians(135))
+                .splineToLinearHeading(new Pose2d(35, 20, Math.toRadians(90)), Math.toRadians(270))
+                .lineToY(51)
+                .lineToY(30)
+                .splineTo(new Vector2d(-15, 15), Math.toRadians(135))
+
+
+
+
+//                .splineTo(new Vector2d(35,38), Math.toRadians(90))
+//                .splineTo(new Vector2d(35,50), Math.toRadians(90))
+//                .splineToLinearHeading(new Pose2d(14,15,Math.toRadians(90)), Math.toRadians(270))
+//                .splineToLinearHeading(new Pose2d(12,55,Math.toRadians(90)), Math.toRadians(270))
+//                .splineToLinearHeading(new Pose2d(-15,-15,Math.toRadians(225)), Math.toRadians(270))
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
